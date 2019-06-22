874fa2a60be37d4634ae3d572112fd1f44840350
Dhananjay kumar <dhananjay.kumar@neudesic.com>
2019-06-22 10:10:05 +0530

    added xunit test project.

---
+++ hotelapi.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace hotelapi.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("A", "A");
+        }
+    }
+}
