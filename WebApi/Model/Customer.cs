9e696a211c7c45451cd1af035ddfb742db47af3d
Dhananjay kumar <dhananjay.kumar@neudesic.com>
2019-07-23 16:27:25 +0530

    fixed.

--- WebApi/Model/Customer.cs
+++ WebApi/Model/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApi.Model
 {
@@ -8,6 +9,7 @@ namespace WebApi.Model
 
         public string Name { get; set; }
 
+        [NotMapped]
         public int Age { get; set; }
 
         public IList<Order> Orders { get; set; }
